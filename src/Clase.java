55212d8379958d4813c186a4876fa4874c76cafe
Andres <animacion96@gmail.com>
2022-12-19 16:29:09 +0100

    corrección menor: escape de comilla

--- src/Clase.java
+++ src/Clase.java
@@ -12,7 +12,7 @@ class Clase {
 
     @Override
     public String toString() {
-        return "Objeto { i=" + entero + ", a='" + string + "\'}";
+        return "Objeto { i=" + entero + ", a='" + string + "'}";
     }
 }
 
