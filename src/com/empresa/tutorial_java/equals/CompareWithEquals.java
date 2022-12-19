b3c157f887ca643127bd8e760c5794a60f2de464
Andres <animacion96@gmail.com>
2022-12-19 16:26:56 +0100

    aordenando en paquetes

--- src/com/empresa/tutorial_java/equals/CompareWithEquals.java
+++ src/com/empresa/tutorial_java/equals/CompareWithEquals.java
@@ -1,15 +1,11 @@
+package com.empresa.tutorial_java.equals;
+
 /**
  * @Author: Andrés Fernández Pereira
  */
-class Persona {
-    private final String nombre;
 
-    public Persona(String nombre) {
-        this.nombre = nombre;
-    }
-}
 
-public class CompareWithEquals {
+class CompareWithEquals {
 
     public static void main(String... argv) {
 
@@ -26,7 +22,7 @@ public class CompareWithEquals {
         String s1 = "hola";
         String s2 = s1;
         String s3 = "hola";
-        String s4 =  new String("hola"); // aunque el IDE sugiere no hacer explicita la instancia,...
+        String s4 = new String("hola"); // aunque el IDE sugiere no hacer explicita la instancia,...
 
         System.out.println(s1 == s2); // true
         System.out.println(s1 == s3); // true
--- src/com/empresa/tutorial_java/paso_valor/paso_objetos/PasoObjetos.java
+++ src/com/empresa/tutorial_java/paso_valor/paso_objetos/PasoObjetos.java
@@ -1,10 +1,10 @@
-/**
+package com.empresa.tutorial_java.paso_valor.paso_objetos; /**
  * @Author: Andrés Fernández Pereira
  */
 import java.util.Arrays;
-public class Main {
-    public static void main(String[] args) {
+public class PasoObjetos {
 
+    public static void main(String[] args) {
 
         System.out.println("\n--- Pasando un int");
         int entero = 1;
@@ -26,37 +26,8 @@ public class Main {
         modificaContenidoObjeto(objeto2); // pasamos la referencia al objeto
         System.out.println("Valor del objeto fuera del método: " + objeto2);
 
-
-        System.out.println("\n--- Pasando arrays");
-
-        int[] array1 = new int[5];
-        printArray(array1);
-        array1[0] = 0;  // innecesario ya que inicializa a 0 por defecto
-
-        int[] array2 = array1;  // dos variables apuntando al mismo array
-        int[] array3 = Arrays.copyOf(array1, array1.length); // dos arrays iguales
-
-        IncrementaArray(array1);
-        IncrementaArray(array2);
-        IncrementaArray(array3);
-
-        printArray(array1);
-        printArray(array2);
-        printArray(array3);
-
     }
 
-    private static void IncrementaArray(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            array[i] = array[i] + 1;
-        }
-    }
-
-
-    private static void printArray(int[] array) {
-        Arrays.stream(array).forEach(System.out::print);
-        System.out.println();
-    }
 
     private static void modificaVarInt(int a) {
         a++; // Incrementamos en 1
